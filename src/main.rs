e526e86aaa5fd4cb4608ff87544fd5694761007c
b b <b@b.b>
2026-05-21 20:15:08 +0300

    actually needed ctrl shift v for terminals

--- src/main.rs
+++ src/main.rs
@@ -804,7 +804,9 @@ fn main() {
 
             let mut enigo = Enigo::new();
             enigo.key_down(Key::Control);
+            enigo.key_down(Key::Shift);
             enigo.key_click(Key::Layout('v'));
+            enigo.key_up(Key::Shift);
             enigo.key_up(Key::Control);
         }
     }
